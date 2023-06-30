8be8db558f7d5f9e6af2f6c005d4e12a76adc353
Erick <126675963+Erick-BFS@users.noreply.github.com>
2023-06-30 16:56:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let contador = 0
 let numero = prompt('escreva um número inteiro e positivo.')
 
-while (contador < numero) {
+while ((contador-1) !=numero) {
+
+    if(numero < 0){
+        alert("Apenas numeros positivos")
+        break;
+    }
+    if(!Number(numero)){
+        alert("Apenas numeros")
+        break;
+    }
+
     contador++
     console.log(contador)
 }
